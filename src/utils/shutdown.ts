1c9acc26a196cb805aba56640369e1aa950f371f
Sahas Saurav <60291223+SahasSaurav@users.noreply.github.com>
2022-10-21 23:33:46 +0530

    Revert "Chore/test setup"

--- src/utils/shutdown.ts
+++
@@ -1,9 +0,0 @@
-import type { FastifyInstance } from 'fastify'
-
-async function gracefulShutdown(signal: string, server: FastifyInstance): Promise<void> {
-	server.log.info(`${signal} signal received and server is shutting down gracefully`)
-	await server.close()
-	process.exit(0)
-}
-
-export { gracefulShutdown }
