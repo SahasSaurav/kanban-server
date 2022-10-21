c4d6e885f9071f2ef8e0e7f8c790749dc69a1b6a
sahas saurav <mike.sahas@gmail.com>
2022-10-21 23:15:47 +0530

    chore(test): added the test environment and setup

--- src/modules/healthcheck/test.ts
+++ src/modules/healthcheck/test.ts
@@ -1,15 +1,19 @@
 import { app } from '../../app'
 
 describe('healthcheck route', () => {
-	it('health check status response code is 200', async () => {
+	it('should health check route response return status  code of 200', async () => {
 		const response = await app.inject({
 			method: 'GET',
 			url: '/api/healthcheck',
+			headers: {
+				accept: 'application/json',
+				'content-type': 'application/json',
+			},
 		})
 
-		const contenType = response.headers['content-type']
+		const contentType = response.headers['content-type']
 
 		expect(response.statusCode).toBe(200)
-		expect(contenType).toBe('application/json; charset=utf-8')
+		expect(contentType).toBe('application/json; charset=utf-8')
 	})
 })
